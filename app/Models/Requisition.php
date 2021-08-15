5a40c47382484c8ddba759211878314388f3bedd
Elshat <elshat90@mail.ru>
2021-08-16 03:07:03 +0600

    answers table created

--- app/Models/Requisition.php
+++ app/Models/Requisition.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Answer;
 
 class Requisition extends Model
 {
     public function user(){
     	return $this->belongsTo(User::class);
+    }
+
+    public function answers(){
+        return $this->hasMany(Answer::class);
     }
 }
--- database/migrations/2021_08_15_205252_create_answers_table.php
+++ database/migrations/2021_08_15_205252_create_answers_table.php
@@ -15,7 +15,12 @@ class CreateAnswersTable extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
+            $table->text('answer');
+            $table->integer('status')->default(0);
+            $table->unsignedBigInteger('requisition_id');
             $table->timestamps();
+
+            $table->foreign('requisition_id')->references('id')->on('requisitions')->onDelete('cascade');
         });
     }
 
