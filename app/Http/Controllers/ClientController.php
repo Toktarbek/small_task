d919a36312d69d41b9b8eca2347ff97797a0ec2a
Elshat <elshat90@mail.ru>
2021-08-16 01:06:23 +0600

    change ClientController

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(Request $request)
     {
         return view('client/index');
